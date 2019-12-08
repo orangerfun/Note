5c766457302fcf403214625b84abd07d82e8e63c
fangcheng <orangerfun@gmail.com>
2019-12-08 12:47:55 +0800

    add scripts

---
+++ script/转换为unicode.py
@@ -0,0 +1,11 @@
+def convert_to_unicode(text):
+# Converts `text` to Unicode (if it's not already), assuming utf-8 input.
+
+    if isinstance(text, str):
+        return text
+    elif isinstance(text, bytes):
+        return text.decode("utf-8", "ignore")
+    else:
+        raise ValueError("Unsupported string type: %s" % (type(text)))
+
+print(convert_to_unicode("７ ０ ０ ０ ０ ０"))
